f0dccfa71003017981e6a4ea26e22cf6d70ba682
suebtas <suebtas@gmail.com>
2017-04-05 22:46:17 +0700

    Revert "start project with oracle-oci"
    This reverts commit 8c9e32ea5a17317c5a66c0a6c8d8b13cb3d7edf4.

---
+++ resources/views/employee/emp.blade.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<script src="//code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css">
+<script>
+    $(document).ready(function (){$('#example').DataTable();});
+</script>
+</head>
+<body>
+    <table id="example">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>aaa</td>
+                <td>bbb</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
