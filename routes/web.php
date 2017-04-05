7a6ab05048603cd1cdbd5d47561e1fc735a4d866
suebtas <suebtas@gmail.com>
2017-04-05 23:58:06 +0700

    setting

--- routes/web.php
+++ routes/web.php
@@ -405,3 +405,26 @@ Route::get('emp', function(){
 </html>';
 });
 
+Route::get('/tns', function(){
+ 
+    $conn = oci_connect('ora1', 'ora1', '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=d1fb7794cd72)(Port=1521))(CONNECT_DATA=(SID=xe)))'); 
+
+    if (!$conn) {
+        $e = oci_error();
+        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    }
+
+    $stid = oci_parse($conn, 'SELECT * FROM EMPLOYEES');
+    oci_execute($stid);
+
+    echo "<table border='1'>\n";
+    while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+        echo "<tr>\n";
+        foreach ($row as $item) {
+            echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+        }
+        echo "</tr>\n";
+    }
+    echo "</table>\n";
+    return;
+});
